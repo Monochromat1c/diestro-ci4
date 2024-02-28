b4ddd8c3c70b7c24a600af8ebc224b8a16e5b4d0
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2024-02-28 13:57:26 +0800

    Added input placeholders to the form

--- app/Views/users/add.php
+++ app/Views/users/add.php
@@ -15,31 +15,31 @@
             <div class="card-body">
                 <div class="mb-3">
                     <label for="first_name">First Name:</label>
-                    <input type="text" class="form-control" id="first_name" name="first_name"/>
+                    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter your first name"/>
                 </div>
                 <div class="mb-3">
                     <label for="middle_name">Middle Name:</label>
-                    <input type="text" class="form-control" id="middle_name" name="middle_name"/>
+                    <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Enter your middle name"/>
                 </div>
                 <div class="mb-3">
                     <label for="last_name">Last Name:</label>
-                    <input type="text" class="form-control" id="last_name" name="last_name"/>
+                    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter your last name"/>
                 </div>
                 <div class="mb-3">
                     <label for="age">Age:</label>
-                    <input type="number" class="form-control" id="age" name="age"/>
+                    <input type="number" class="form-control" id="age" name="age" placeholder="Enter your age"/>
                 </div>
                 <div class="mb-3">
                     <label for="email">Email:</label>
-                    <input type="text" class="form-control" id="email" name="email"/>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email"/>
                 </div>
                 <div class="mb-3">
                     <label for="password">Password:</label>
-                    <input type="password" class="form-control" id="password" name="password"/>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password"/>
                 </div>
                 <div class="mb-3">
                     <label for="password">Confirm Password:</label>
-                    <input type="password" class="form-control" id="password" name="password"/>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Confirm your password"/>
                 </div>
                 <div class="col-12 col-sm-3 float-end">
                     <button id="btn" type="submit" class="btn btn-success w-100">Save</button>
